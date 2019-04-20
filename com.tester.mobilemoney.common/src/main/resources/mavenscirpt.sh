1ea3c58d45c098267ffcd0971fbfa6f327dc8a51
Amit Singh <amit.singh5@synapse.com>
2019-04-20 12:14:50 +0530

    Commited Testing Project

---
+++ com.tester.mobilemoney.common/src/main/resources/mavenscirpt.sh
@@ -0,0 +1,9 @@
+mvn archetype:generate -DgroupId=com.tester.mobielmoney.top -DartifactId=compile -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+
+    <groupId>com.tester.mobile.money</groupId>
+    <version>1.0-SNAPSHOT</version>
+    <artifactId>bundle</artifactId>
+    <name>${project.groupId}/${project.artifactId}</name>
+    <description>${project.name} definition</description>jj
+    <packaging>pom</packaging>
---
+++ com.tester.mobilemoney.persistance/persistance.api/src/main/java/com/tester/mobilemoney/com/tester/mobilemoney/persistance/imp/Party.java
@@ -0,0 +1,9 @@
+package com.tester.mobilemoney.com.tester.mobilemoney.persistance.imp;
+
+import java.util.List;
+
+public interface Party {
+    public Party getParty();
+
+
+}
